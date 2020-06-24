a8b25840a7bb313bd65d9a601a94b2e4b2c33635
Cedric Ahlers <Ceddy3301@outlook.de>
2020-06-24 22:18:09 +0200

    Reset to newest local version

--- Detektivspiel/Form3.Designer.cs
+++ Detektivspiel/Form3.Designer.cs
@@ -36,9 +36,10 @@
             // 
             this.ok_btn_msgbox.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.ok_btn_msgbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ok_btn_msgbox.Location = new System.Drawing.Point(340, 128);
+            this.ok_btn_msgbox.Location = new System.Drawing.Point(681, 245);
+            this.ok_btn_msgbox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.ok_btn_msgbox.Name = "ok_btn_msgbox";
-            this.ok_btn_msgbox.Size = new System.Drawing.Size(68, 21);
+            this.ok_btn_msgbox.Size = new System.Drawing.Size(136, 40);
             this.ok_btn_msgbox.TabIndex = 0;
             this.ok_btn_msgbox.Text = "OK";
             this.ok_btn_msgbox.UseVisualStyleBackColor = true;
@@ -47,21 +48,27 @@
             // 
             this.lbl_msgbox.BackColor = System.Drawing.SystemColors.Info;
             this.lbl_msgbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_msgbox.Location = new System.Drawing.Point(33, 13);
+            this.lbl_msgbox.Location = new System.Drawing.Point(29, 25);
+            this.lbl_msgbox.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbl_msgbox.Name = "lbl_msgbox";
-            this.lbl_msgbox.Size = new System.Drawing.Size(375, 101);
+            this.lbl_msgbox.Size = new System.Drawing.Size(788, 194);
             this.lbl_msgbox.TabIndex = 1;
             this.lbl_msgbox.Text = "Text_Messagebox";
             // 
             // MsgBox
             // 
             this.AcceptButton = this.ok_btn_msgbox;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 161);
+            this.ClientSize = new System.Drawing.Size(854, 309);
+            this.ControlBox = false;
             this.Controls.Add(this.lbl_msgbox);
             this.Controls.Add(this.ok_btn_msgbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MinimumSize = new System.Drawing.Size(880, 380);
             this.Name = "MsgBox";
+            this.Padding = new System.Windows.Forms.Padding(3);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "MsgBox";
             this.Load += new System.EventHandler(this.Form3_Load);
